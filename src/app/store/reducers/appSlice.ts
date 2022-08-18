6f49a4f8533701893aa33b0143a870bb75717ef2
Kristen <kr1s10.khud@gmail.com>
2022-08-19 02:17:11 +0300

    feat: add routing for audio trainer

--- src/app/store/reducers/appSlice.ts
+++ src/app/store/reducers/appSlice.ts
@@ -2,22 +2,29 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export interface IAppState {
   value: number;
+  audioLesson: number;
+  textLesson: number;
 }
 
 const initialState: IAppState = {
   value: 0,
+  audioLesson: 1,
+  textLesson: 1,
 };
 
 export const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
-    test: (state) => {
-      console.log(state.value);
+    setAudioLesson: (state, action) => {
+      state.audioLesson = action.payload.lesson;
+    },
+    setTextLesson: (state, action) => {
+      state.textLesson = action.payload.lesson;
     },
   },
 });
 
-export const { test } = appSlice.actions;
+export const { setAudioLesson, setTextLesson } = appSlice.actions;
 
 export default appSlice.reducer;
