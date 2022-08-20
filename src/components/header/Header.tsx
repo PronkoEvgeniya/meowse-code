bf3bcc4cd045c33e422bc8db435294a82807a6d4
Kristen <kr1s10.khud@gmail.com>
2022-08-20 18:29:21 +0300

    fix: header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks/reduxHooks';
 import logo from './logo.svg';
-import { User } from './User';
-import { UserIcon } from './UserIcon';
+// import { User } from './User';
+// import { UserIcon } from './UserIcon';
 
 export const Header = (): JSX.Element => {
   const auth = useAppSelector(({ app: { isAuthorized } }) => isAuthorized);
@@ -15,8 +15,28 @@ export const Header = (): JSX.Element => {
             <img src={logo} alt="logo" />
           </Link>
         </li>
-        <li>{auth ? <User /> : <UserIcon />}</li>
+        <li>
+          <div
+            style={{ width: '55px', height: '55px', background: 'purple' }}
+            className="user-icon"
+          ></div>
+          <span>name</span>
+          <div
+            style={{
+              width: '30px',
+              height: '30px',
+              borderBottom: '5px solid purple',
+              borderRight: '5px solid purple',
+              transform: 'rotate(45deg)',
+            }}
+            className="user-arrow"
+          ></div>
+        </li>
       </ul>
     </header>
   );
 };
+
+{
+  /* <li>{auth ? <User /> : <UserIcon />}</li> */
+}
