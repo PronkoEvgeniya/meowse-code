513bd0c0b1e9e48591c9b66e7e09e10fca9bc207
PronkoEvgeniya <Shreidereva@gmail.com>
2022-08-26 18:56:10 +0300

    feat: add assets for eng testing

--- src/assets/audio/test/index.ts
+++ src/assets/audio/test/index.ts
@@ -4,4 +4,9 @@ export const test = {
   third: './3ru.np3',
   fourth: './4ru.np3',
   fifth: './5ru.np3',
+  sixth: './1en.np3',
+  seventh: './2en.np3',
+  eighth: './3en.np3',
+  ninth: './4en.np3',
+  tenth: './5en.np3',
 };
