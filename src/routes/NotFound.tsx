d45e257b2da1845f321e449fc31bf4347cabfbad
PronkoEvgeniya <Shreidereva@gmail.com>
2022-09-05 17:22:32 +0300

    fix: icons on start page

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,3 +1,17 @@
+import './notFound.scss';
+import mascot from '../assets/images/scared-meows.png';
+import { useTranslation } from 'react-i18next';
+import { useContext } from 'react';
+import { DarkThemeContext } from '../context/DarkModeContext';
+
 export const NotFound = (): JSX.Element => {
-  return <div>404</div>;
+  const { t } = useTranslation();
+  const { darkTheme } = useContext(DarkThemeContext);
+
+  return (
+    <div className="not-found__page">
+      <img src={mascot} alt="" />
+      <p className={darkTheme ? 'notfound__text dark' : 'notfound__text'}>{t('notfound')}</p>
+    </div>
+  );
 };
